a9d636d21102c2dbfe9dfb4f34d63fbe80d70ab1
Davide Garberi <dade.garberi@gmail.com>
2020-09-11 16:32:23 +0200

    msm8996-common: Add default dalvik heap values as fallback
    * In case libinit doesn't get loaded (for example on any pure AOSP GSI) we
      need to have fallback values to make the device usable
    
    Change-Id: I149be63a625d3b00188512b45fe0e556de6abaec

--- msm8996.mk
+++ msm8996.mk
@@ -29,6 +29,9 @@ $(call inherit-product, vendor/zuk/msm8996-common/msm8996-common-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
